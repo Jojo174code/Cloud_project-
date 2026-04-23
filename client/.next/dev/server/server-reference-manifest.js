dadac755efc18eb1ce5a6720935f3dabe984dd20
Josiah <Josiahrhodes160@gmail.com>
2026-04-23 15:32:07 -0500

    new and working on local machine

--- client/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0SJ86jzuXUrrq7xVSGpIagGbaD1AOAhVRzhhI1MnbZQ=\"\n}"
--- client/.next/dev/static/chunks/client_src_app_layout_tsx_0bcr7sh._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/client_src_styles_globals_0.ap_vv.css","static/chunks/059b_next_0.mq11l._.js","static/chunks/client_src_0inge9j._.js"],
-    source: "dynamic"
-});
